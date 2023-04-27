59929229d166641dc6e0a58d96e04ac7cfbb45c7
Marv0912 <marvin.fajardo.s@icloud.com>
2023-04-27 18:28:50 -0400

    tweet functionality and bug fixes

--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home');
 
+
 router.get('/', (req, res) => {
-    res.render('home/new');
+    res.render('home/index');
 });
+
+
 //router.get('/home', homeController.displayTweets);
 
 // TODO: remember to place path and to put route on server.js
