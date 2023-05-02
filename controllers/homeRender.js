8ca66d8d437a16acbd9fbd15860dc2d9f517c556
Marv0912 <marvin.fajardo.s@icloud.com>
2023-05-02 11:37:15 -0400

    tweets reverse chronological order

--- controllers/homeRender.js
+++ controllers/homeRender.js
@@ -5,7 +5,7 @@ const createTweet = require('./home')
 exports.renderHome = async (req, res, tweets) => {
     try {
         console.log(req.params);
-        const tweets = await Tweet.find({})
+        const tweets = await Tweet.find({}).sort({ createdAt: 'desc' });
         res.render('home/index', { tweets, username: req.params.username });
     } catch (error) {
         console.error(error);
